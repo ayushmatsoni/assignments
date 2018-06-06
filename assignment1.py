2221845e9f7c4be85fcc598bf753323ec2fdaab3
ayushmatsoni <ayushmats@gmail.com>
2018-06-06 19:39:15 +0530

    starting assignments

--- assignment1.py
+++ assignment1.py
@@ -1,1 +1,22 @@
 print("hello")
+
+print("ACAD"+"VIEW")
+
+a=10
+b=20
+c=30
+print("the value of a, b, c==%d%d%d"%(a,b,c))
+
+
+print (' " let\'s get started " ')
+
+
+s="acadview"
+course="python"
+fees=5000
+print("welcome to %s , your course is %s and fees is %d"%(s,course,fees))
+
+name="tony"
+salary=50000
+print('%s%d'%(name,salary))
+
